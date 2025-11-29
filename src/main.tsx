aadf4c0f3e1702c3bf6ed8371bd7a17f550ea154
unknown <ssencarnacion1@up.edu.ph>
2025-11-29 11:04:21 +0800

    feat: implemented analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Intro from "./intro";
 import Projects from "./projects";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Intro />
     <Projects />
+    <Analytics />
   </StrictMode>
 );
